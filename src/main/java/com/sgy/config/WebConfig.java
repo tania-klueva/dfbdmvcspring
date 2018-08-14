29d4a840026a1b014e94b046925b16233ff7082a
tania-klueva <tania.kliuieva@gmail.com>
2018-08-14 15:36:00 +0300

    add resolver

--- src/main/java/com/sgy/config/WebConfig.java
+++ src/main/java/com/sgy/config/WebConfig.java
@@ -1,8 +1,23 @@
 package com.sgy.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+@ComponentScan("com.sgy.*")
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
